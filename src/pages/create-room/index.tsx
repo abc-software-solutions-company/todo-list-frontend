3da7492837efef353d89895c6fea06c4a21abff3
ThanhTuyen <110363843+GreencrisMessi@users.noreply.github.com>
2022-08-18 13:35:00 +0700

    feat: 🎸 TODO-16 - Create List or Join room with existing  ID

--- src/pages/create-room/index.tsx
+++ src/pages/create-room/index.tsx
@@ -1,7 +1,4 @@
-import Image from 'next/image';
 import React from 'react';
-
-import logoRoom from '@/assets/images/logo-ABC-company.png';
 
 import styles from './style.module.scss';
 
@@ -11,16 +8,13 @@ const CreateRoom = () => {
       <div className={styles['create-room']}>
         <div className="container">
           <div className="section-room">
-            <div className="logo-abc">
-              <Image src={logoRoom} alt="Logo Room" width={279} />
-            </div>
             <div className="title-room">
               <p className="title-todo">TO DO LIST</p>
               <p className="description-todo">Organize your work and life, finally.</p>
             </div>
             <div className="section-btn">
               <div className="btn">
-                <button className="title-btn">+ Create New List</button>
+                <button className="title-btn">Create New List</button>
               </div>
               <div className="input-group-room  ">
                 <input type="text" className="form-control-room" placeholder="Enter link or ID" />
