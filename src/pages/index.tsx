905c29313f44f3b8a21a60a2d9593ca5c776f278
ThanhTuyen <110363843+GreencrisMessi@users.noreply.github.com>
2022-08-18 15:29:56 +0700

    feat: 🎸 TODO-17 - Create Page 02 - List

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import {getStaticProps} from '@/data/ssr/home.ssr';
 import LayoutDefault from '@/layouts/default';
 
-import CreateList from './create-list';
 import HomePage from './home';
 
 export {getStaticProps};
@@ -12,7 +11,6 @@ export {getStaticProps};
 export default function PageHome({}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
-      <CreateList />
       <HomePage />
     </>
   );
