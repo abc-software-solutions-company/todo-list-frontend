7ec67b89bf4271b05a4df1cccffe36e5d74af919
lamminhthien <lamminhthien02012000@gmail.com>
2022-09-28 10:57:21 +0700

    add open google popup

--- src/components/modal-social/index.tsx
+++ src/components/modal-social/index.tsx
@@ -2,6 +2,7 @@ import cls from 'classnames';
 import React from 'react';
 
 import Button from '@/core-ui/button';
+import {FireAuthUtils} from '@/lib/firebase/fireAuth-utils';
 
 import {Modal} from '../../core-ui/modal';
 import styles from './style.module.scss';
@@ -10,12 +11,17 @@ interface IProps {
   open: boolean;
   onClose: () => void;
 }
+const fireAuthUtils = new FireAuthUtils();
+const openGooglePopUp = () => {
+  fireAuthUtils.signInWithGoogle();
+};
+
 const ModalSocial: React.FC<IProps> = ({open, onClose}) => {
   return (
     <Modal variant="center" className={cls(styles['com-modal-social'], 'max-w-[378px]')} open={open} onClose={onClose}>
       <Modal.Header />
       <Modal.Body className="container">
-        <Button className=" bg-white text-black">
+        <Button className=" bg-white text-black" onClick={() => openGooglePopUp()}>
           <img src="/google.png" />
           <span>Sign in with Google</span>
         </Button>
