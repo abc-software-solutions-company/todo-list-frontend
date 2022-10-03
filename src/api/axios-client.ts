311c669ddf4984c1388601afc6f5a98dfe644ed0
lamminhthien <lamminhthien02012000@gmail.com>
2022-10-03 16:19:17 +0700

    make advanced userflow to handle anonymous user in axios interceptor

--- src/api/axios-client.ts
+++ src/api/axios-client.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+import TaskAPI from '@/api/network/todo';
+import UserAPI from '@/api/network/user';
 import {ROUTES} from '@/configs/routes.config';
 import LocalStorage from '@/utils/local-storage';
 
@@ -34,7 +36,33 @@ axiosClient.interceptors.response.use(
   err => {
     if (err?.response?.status === 401) {
       if (typeof window !== 'undefined') {
-        window.location.href = ROUTES.LOGIN;
+        // Lấy đường dẫn hiện tại
+        const currentPath = window.location.href;
+        // Kiểm tra xem đây có phải là đường dẫn đến list detail không
+        const isListDetail = currentPath.includes(`${ROUTES.LIST}/`);
+        // Nếu là đường dẫn đến list detail
+        if (isListDetail) {
+          // Lấy ID list ra
+          const listID = currentPath.split('/')[4];
+          // Nếu ID list không hợp lệ (undenfined hoặc số lượng kí tự  không phù hợp {không đủ 5})
+          if (listID == undefined || listID.length != 5) {
+            console.log('😓😓 ListID không phù hợp');
+          } else {
+            // Nếu đúng định dạng listID thì tiến hành kiểm tra xem phòng có tồn tại hay ko?
+            TaskAPI.getTodo(listID).then(res => {
+              // Nếu lấy list thành công thì in ra thử, sẽ tạo anonymous user chỗ này
+              console.log(res.data);
+              // Sau đó ta sẽ gọi API để tạo anonymous user
+              // Có thể tạo tên theo cú pháp anonymous + "Random Character" + "Random Emoji"
+              UserAPI.createUser({userName: 'anonymous 😎'});
+            });
+            console.log('🤩🤩🤩 list ID đúng định dạng');
+          }
+        }
+
+        // console.log(listID == );
+
+        // window.location.href = ROUTES.LOGIN;
         LocalStorage.accessToken.remove();
       }
     }
