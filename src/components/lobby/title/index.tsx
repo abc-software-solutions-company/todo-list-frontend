3791889ba570b3a4b362e87b55d7b8359fb7f524
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-10-27 11:12:19 +0700

    split and optimize code for lobby text

--- src/components/lobby/title/index.tsx
+++ src/components/lobby/title/index.tsx
@@ -1,12 +1,15 @@
 import {FC} from 'react';
 
+import {LobbyTexts} from '@/utils/constant';
+
 import styles from './style.module.scss';
 
 const LobbyTitle: FC = () => {
   return (
     <>
-      <p className={styles.title}>TO-DO LIST</p>
-      <p className={styles.h1}>Organize your work and life, finally.</p>
+      <p className={styles.title}>{LobbyTexts.TITLE}</p>
+      <p className={styles.headline}>{LobbyTexts.HEADLINE}</p>
+      <p className={styles.quote}>{LobbyTexts.QUOTE}</p>
     </>
   );
 };
