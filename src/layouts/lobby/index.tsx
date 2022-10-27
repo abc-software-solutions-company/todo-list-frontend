0cd43f517f1fe9c57d782cf52724590507d4e057
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-10-28 06:56:13 +0700

    fix background

--- src/layouts/lobby/index.tsx
+++ src/layouts/lobby/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import Footer from '@/components/footer';
 import TopBarLobby from '@/components/lobby/topbar';
@@ -6,13 +6,21 @@ import TopBarLobby from '@/components/lobby/topbar';
 import styles from './style.module.scss';
 
 export default function LobbyLayout({children}: React.PropsWithChildren<Record<string, unknown>>) {
+  useEffect(() => {
+    if (window !== undefined) {
+      document.getElementsByTagName('body')[0].classList.add("bg-[url('/image/bg-lobby.png')]");
+      console.log('render background because scrollbar come from body');
+    }
+  });
   return (
-    <div className={styles['layout-lobby']}>
-      <TopBarLobby />
-      <main>{children}</main>
-      <div className="bg-transparent">
-        <Footer />
+    <>
+      <div className={styles['layout-lobby']}>
+        <TopBarLobby />
+        <main>{children}</main>
+        <div className="bg-transparent">
+          <Footer />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
