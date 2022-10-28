eb3ff9fa206f45cfcf13207472166d24ca915ee4
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-10-28 09:30:15 +0700

    .

--- src/layouts/lobby/index.tsx
+++ src/layouts/lobby/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 
 import Footer from '@/components/footer';
 import TopBarLobby from '@/components/lobby/topbar';
@@ -6,14 +6,15 @@ import TopBarLobby from '@/components/lobby/topbar';
 import styles from './style.module.scss';
 
 export default function LobbyLayout({children}: React.PropsWithChildren<Record<string, unknown>>) {
-  useEffect(() => {
-    if (window !== undefined) {
-      document.getElementsByTagName('body')[0].classList.add("bg-[url('/image/bg-lobby.png')]");
-      console.log('render background because scrollbar come from body');
-    }
-  });
+  // useEffect(() => {
+  //   if (window !== undefined) {
+  //     document.getElementsByTagName('body')[0].classList.add("bg-[url('/image/bg-lobby.png')]");
+  //     console.log('render background because scrollbar come from body');
+  //   }
+  // });
   return (
     <>
+      <img className="absolute left-0 top-0 h-full w-full object-cover object-center" src="/image/bg-lobby.png" alt="" />
       <div className={styles['layout-lobby']}>
         <TopBarLobby />
         <main>{children}</main>
