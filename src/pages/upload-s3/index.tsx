c0ccf42bd5b953feb0061f5b0cd0704c616b92ef
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-10-28 15:50:33 +0700

    upload file

--- src/pages/upload-s3/index.tsx
+++ src/pages/upload-s3/index.tsx
@@ -1,10 +1,18 @@
+import aws from 'aws-sdk';
 import React from 'react';
 import {Resolver, useForm} from 'react-hook-form';
+
+aws.config.update({
+  accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY
+});
+
+const s3 = new aws.S3();
 
 type FormValues = {
   firstName: string;
   lastName: string;
-  image: any;
+  image: File[];
 };
 
 const resolver: Resolver<FormValues> = async values => {
@@ -27,7 +35,18 @@ export default function UploadImage() {
     handleSubmit,
     formState: {errors}
   } = useForm<FormValues>({resolver});
-  const onSubmit = handleSubmit(data => console.log(data));
+  const onSubmit = handleSubmit(data => {
+    const file = data.image;
+    console.log(file[0].name);
+
+    const params = {
+      Bucket: 'todo-list-website-production',
+      // Body: fs.createReadStream(filePath),
+      Key: `image/${data.image}`,
+      ACL: 'public-read'
+    };
+    console.log(params);
+  });
 
   return (
     <form onSubmit={onSubmit}>
