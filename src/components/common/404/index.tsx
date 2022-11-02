3b47f889cb2bfed9d479b1838916700c0502481f
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-11-02 20:28:50 +0700

    add custom 404 page

--- src/components/common/404/index.tsx
+++ src/components/common/404/index.tsx
@@ -15,9 +15,8 @@ const ErrorInformation: React.FC = () => {
       <Seo title={'404 Not Found'} />
       <div className={styles['page-not-found']}>
         <div className="container">
-          <p className="heading">{t('404-heading')}</p>
+          <p className="heading">404</p>
           <h1 className="sub-heading">{t('404-sub-heading')}</h1>
-          <div className="image">404 NOT FOUND</div>
           <Link href={ROUTES.HOME}>{t('404-back-home')}</Link>
         </div>
       </div>
