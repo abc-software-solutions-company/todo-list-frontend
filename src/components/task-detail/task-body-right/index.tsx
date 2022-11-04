b921d7e8d2a5c19fa441c6ae34defc7dda6c8b9a
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-11-04 11:28:14 +0700

    style task right body now look goodf

--- src/components/task-detail/task-body-right/index.tsx
+++ src/components/task-detail/task-body-right/index.tsx
@@ -1,6 +1,7 @@
 import {SelectChangeEvent} from '@mui/material';
 
 import Status from '@/components/list-detail/status';
+import Icon from '@/core-ui/icon';
 import {ITaskResponse} from '@/data/api/types/task.type';
 
 import style from './style.module.scss';
@@ -21,30 +22,40 @@ export const TaskBodyRight = ({onChange, taskData}: IBodyRightProp) => {
           </div>
           <div className="assigne">
             <p className="title">Assignee</p>
-            <p>Thien</p>
+            <div className="user">
+              <Icon name="ico-user" />
+              <p>Thien</p>
+            </div>
           </div>
           <div className="piority">
             <p className="title">Piority</p>
-            <p>Medium</p>
+            <div className="piority-type">
+              <Icon name="ico-arrow-up" className="text-green-500" />
+              <p>Medium</p>
+            </div>
           </div>
           <div className="point">
             <p className="title">Point</p>
-            <p>8</p>
+            <input type="number" id="quantity" name="quantity" min="1" max="5" />
           </div>
           <div className="date">
-            <div className="date-start">
-              <p className="title">Start date</p>
-              <p>3/11/2022</p>
+            <div className="start-due">
+              <div className="date-start">
+                <p className="title">Start date</p>
+                <p>3/11/2022</p>
+              </div>
+              <div className="date-due">
+                <p className="title">Due date</p>
+                <p>4/11/2022</p>
+              </div>
             </div>
-            <div className="date-due">
-              <p className="title">Due date</p>
-              <p>4/11/2022</p>
-            </div>
-            <div className="date-create">
-              <p>Created November 1, 2022, 9:33 AM</p>
-            </div>
-            <div className="date-update">
-              <p>Updated November 2, 2022, 1:33 PM</p>
+            <div className="create-update">
+              <div className="date-create">
+                <p>Created November 1, 2022, 9:33 AM</p>
+              </div>
+              <div className="date-update">
+                <p>Updated November 2, 2022, 1:33 PM</p>
+              </div>
             </div>
           </div>
         </div>
