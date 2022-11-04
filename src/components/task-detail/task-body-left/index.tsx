b2ee91d3b956d3f06c7aa365f142d9b113a330aa
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-11-04 08:27:50 +0700

    prepare for split code to more component

---
+++ src/components/task-detail/task-body-left/index.tsx
@@ -0,0 +1,5 @@
+import {FC} from 'react';
+
+export const TaskBodyLeft: FC = () => {
+  return <>TaskBodyLeft</>;
+};
---
+++ src/components/task-detail/task-description/index.tsx
@@ -0,0 +1,5 @@
+import {FC} from 'react';
+
+export const TaskDescription: FC = () => {
+  return <>TaskDescription</>;
+};
