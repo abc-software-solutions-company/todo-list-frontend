5300122f52e39ecddebcbc486a14077ae1b9788e
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-11-04 10:23:48 +0700

    split task body left component

--- src/components/task-detail/task-body-left/index.tsx
+++ src/components/task-detail/task-body-left/index.tsx
@@ -1,9 +1,10 @@
 import {TextField} from '@mui/material';
-import {useState} from 'react';
+import {ChangeEvent, useState} from 'react';
 
 import UploadImage from '@/components/task-detail/upload-image';
 import Button from '@/core-ui/button';
 import Icon from '@/core-ui/icon';
+import useToast from '@/core-ui/toast';
 import {IAttachment, IAttachmentResponse, ITaskResponse} from '@/data/api/types/task.type';
 
 import {TaskDescription} from '../task-description';
@@ -12,15 +13,41 @@ import TaskImages from '../task-images';
 interface ITaskBodyLeftProp {
   taskData: ITaskResponse;
   updateTaskData: () => void;
-  onUpload: () => void;
-  onSuccess: () => void;
-  onError: () => void;
 }
 
-export const TaskBodyLeft = ({taskData, updateTaskData, onSuccess, onUpload, onError}: ITaskBodyLeftProp) => {
+export const TaskBodyLeft = ({taskData, updateTaskData}: ITaskBodyLeftProp) => {
+  const toast = useToast();
+
   const [previewImages, setPreviewImages] = useState<IAttachment[]>([]);
 
   const taskImages = taskData.taskAttachments?.filter(e => e.isActive).map(e => e.attachment);
+
+  const onUpload = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files?.length) {
+      const images = [];
+      for (let i = 0; i < e.target.files.length; i++) {
+        const element = e.target.files[i];
+        if (element.type.startsWith('image')) images.push({name: element.name, link: URL.createObjectURL(element)});
+        else {
+          toast.show({type: 'danger', title: 'Error', content: 'Warning, this is not image file'});
+        }
+      }
+      setPreviewImages(images);
+    }
+  };
+
+  const onSuccess = () => {
+    console.log('🚀 ~ file: index.tsx ~ line 62 ~ onSuccess ~ onSuccess');
+    updateTaskData();
+    setPreviewImages([]);
+    toast.show({type: 'success', title: 'success', content: 'Update Image Successfull'});
+  };
+
+  const onError = () => {
+    console.log('🚀 ~ file: index.tsx ~ line 67 ~ onError ~ onError', onError);
+    setPreviewImages([]);
+    toast.show({type: 'danger', title: 'Error', content: 'Warning your file must be image and maximum size is 5MB'});
+  };
 
   return (
     <div className="task-body-left">
--- src/components/task-detail/task-body/index.tsx
+++ src/components/task-detail/task-body/index.tsx
@@ -1,18 +1,13 @@
-import {SelectChangeEvent, TextField} from '@mui/material';
+import {SelectChangeEvent} from '@mui/material';
 import classNames from 'classnames';
-import {ChangeEvent, FC, useState} from 'react';
+import {FC} from 'react';
 
-import UploadImage from '@/components/task-detail/upload-image';
-import Button from '@/core-ui/button';
-import Icon from '@/core-ui/icon';
-import useToast from '@/core-ui/toast';
 import api from '@/data/api';
-import {IAttachment, IAttachmentResponse, ITaskResponse} from '@/data/api/types/task.type';
+import {ITaskResponse} from '@/data/api/types/task.type';
 import {socketUpdateList} from '@/data/socket';
 
+import {TaskBodyLeft} from '../task-body-left';
 import {TaskBodyRight} from '../task-body-right';
-import {TaskDescription} from '../task-description';
-import TaskImages from '../task-images';
 import style from './style.module.scss';
 
 interface ITaskBodyProps {
@@ -22,38 +17,6 @@ interface ITaskBodyProps {
 }
 
 const TaskBody: FC<ITaskBodyProps> = ({taskData, updateTaskData, className}) => {
-  const [previewImages, setPreviewImages] = useState<IAttachment[]>([]);
-
-  const toast = useToast();
-
-  const onUpload = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files?.length) {
-      const images = [];
-      for (let i = 0; i < e.target.files.length; i++) {
-        const element = e.target.files[i];
-        if (element.type.startsWith('image')) images.push({name: element.name, link: URL.createObjectURL(element)});
-        else {
-          toast.show({type: 'danger', title: 'Error', content: 'Warning, this is not image file'});
-        }
-      }
-      setPreviewImages(images);
-    }
-  };
-
-  const onSuccess = () => {
-    console.log('🚀 ~ file: index.tsx ~ line 62 ~ onSuccess ~ onSuccess');
-    updateTaskData();
-    setPreviewImages([]);
-    toast.show({type: 'success', title: 'success', content: 'Update Image Successfull'});
-  };
-
-  const onError = () => {
-    console.log('🚀 ~ file: index.tsx ~ line 67 ~ onError ~ onError', onError);
-    setPreviewImages([]);
-    toast.show({type: 'danger', title: 'Error', content: 'Warning your file must be image and maximum size is 5MB'});
-  };
-  const taskImages = taskData.taskAttachments?.filter(e => e.isActive).map(e => e.attachment);
-
   const onChangeStatus = (event: SelectChangeEvent<unknown>) => {
     api.task
       .update({id: taskData.id, statusId: Number(event.target.value)})
@@ -64,6 +27,7 @@ const TaskBody: FC<ITaskBodyProps> = ({taskData, updateTaskData, className}) => 
 
   return (
     <div className={classNames(style['task-body'], className)}>
+      <TaskBodyLeft taskData={taskData} updateTaskData={updateTaskData} />
       <TaskBodyRight onChange={onChangeStatus} taskData={taskData} />
     </div>
   );
