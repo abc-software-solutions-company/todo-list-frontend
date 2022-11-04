717836dd2ddf25b02b59f5b0cf30f276fc53aacd
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-11-04 08:19:59 +0700

    add more content ready for style right column

--- src/components/task-detail/task-body/index.tsx
+++ src/components/task-detail/task-body/index.tsx
@@ -133,7 +133,37 @@ const TaskBody: FC<ITaskBodyProps> = ({taskData, updateTaskData, className}) => 
         </form>
       </div>
       <div className="right">
-        <Status className={style.status} status={taskData.status} items={taskData.todolist.status} onChange={onChangeStatus} />
+        <div className="status">
+          <p>Status</p>
+          <Status className={style.status} status={taskData.status} items={taskData.todolist.status} onChange={onChangeStatus} />
+          <hr />
+        </div>
+        <div className="assigne">
+          <p>Thien</p>
+          <hr />
+        </div>
+        <div className="piority">
+          <p>Medium</p>
+        </div>
+        <div className="story-point">
+          <p>8</p>
+          <hr />
+        </div>
+        <div className="date">
+          <div className="date-start">
+            <p>3/11/2022</p>
+          </div>
+          <div className="date-due">
+            <p>4/11/2022</p>
+          </div>
+          <hr />
+          <div className="date-create">
+            <p>Created November 1, 2022, 9:33 AM</p>
+          </div>
+          <div className="date-update">
+            <p>Updated November 2, 2022, 1:33 PM</p>
+          </div>
+        </div>
       </div>
     </div>
   );
