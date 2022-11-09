9e99677950f8703d20a3f2ab5f359e6e34fc4077
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-11-09 19:11:37 +0700

    remove navigation button

--- src/components/common/popup-img-dangerous/index.tsx
+++ src/components/common/popup-img-dangerous/index.tsx
@@ -7,20 +7,30 @@ interface IPopUpImgProp {
   rawHTML: string;
 }
 
+const customLightboxHTML = `<div id="glightbox-body" class="glightbox-container">
+    <div class="gloader visible"></div>
+    <div class="goverlay"></div>
+    <div class="gcontainer">
+    <div id="glightbox-slider" class="gslider"></div>
+    <button class="gclose gbtn" tabindex="2" aria-label="Close">{closeSVG}</button>
+</div>
+</div>`;
+
 const PopUpImageDangerous = ({rawHTML}: IPopUpImgProp) => {
   useEffect(() => {
     const glightbox = import(/* webpackChunkName: "vendor.glightbox" */ 'glightbox');
-    import(/* webpackChunkName: "vendor.glightbox.style" */ 'glightbox/dist/css/glightbox.min.css');
+    // import(/* webpackChunkName: "vendor.glightbox.style" */ 'glightbox/dist/css/glightbox.min.css');
     glightbox.then(resp => {
       const gLightbox = resp.default;
-      setTimeout(
-        () =>
-          gLightbox({
-            selector: '.glightbox',
-            loop: true
-          }),
-        500
-      );
+      setTimeout(() => {
+        gLightbox({
+          selector: '.glightbox-danger',
+          lightboxHTML: customLightboxHTML,
+          loop: false,
+          keyboardNavigation: false,
+          draggable: false
+        });
+      }, 500);
     });
   }, []);
   return (
