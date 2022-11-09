099c99e557fa0a94422c597f5a1baac9dc843207
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-11-09 11:53:04 +0700

    improve dom event

--- src/pages/popup-image-in-comment/index.tsx
+++ src/pages/popup-image-in-comment/index.tsx
@@ -1,10 +1,25 @@
+import Image from 'next/image';
 import React from 'react';
+
+import PopUpImage from '@/components/common/popup-img';
 
 import {extractUrl} from './utils/extract-url';
 import {rawDataTest} from './utils/rawData';
 
-console.log(extractUrl(rawDataTest));
+const listImageFromComment = extractUrl(rawDataTest);
+console.log(listImageFromComment);
 
 export default function PopUpImageInComment() {
-  return <div>aaaa</div>;
+  return (
+    <div>
+      {listImageFromComment.map((e, idx) => (
+        <>
+          <PopUpImage imageList={[e]}>
+            <Image src={e} key={idx} width={400} height={400} alt={idx.toString()} />
+          </PopUpImage>
+        </>
+      ))}
+      <div className="description-text prose" dangerouslySetInnerHTML={{__html: rawDataTest}}></div>
+    </div>
+  );
 }
