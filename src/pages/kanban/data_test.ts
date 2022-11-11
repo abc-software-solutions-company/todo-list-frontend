c4d1ef1dd4ec33577e135a48ddb27eb5818aa06d
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-11-11 12:04:28 +0700

    fix type

--- src/pages/kanban/data_test.ts
+++
@@ -1,6 +0,0 @@
-export const dataTest = {
-  group1: ['1', '2', '3'],
-  group2: ['4', '5', '6'],
-  group3: ['7', '8', '9'],
-  group4: ['7', '8', '9']
-};
--- src/pages/kanban/index.tsx
+++ src/pages/kanban/index.tsx
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 import React from 'react';
 
-import {dataTest} from './data_test';
+import {dataTest} from '../../utils/data_test_kanban';
 
 const Kanban = dynamic(() => import('@/components/common/kanban'), {
   ssr: false
@@ -10,7 +10,7 @@ const Kanban = dynamic(() => import('@/components/common/kanban'), {
 export default function KanbanPage() {
   return (
     <div>
-      Kanabn
+      Kanban Component, Send data via prop
       <Kanban data={dataTest} />
     </div>
   );
