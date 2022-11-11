18cb5eb55a3191ff28d81d88f439c375e841d97b
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-11-11 13:49:20 +0700

    🐛 Bỏ bớt chữ more detailed

--- src/components/task-detail/task-body/body-left/description/description-button/index.tsx
+++ src/components/task-detail/task-body/body-left/description/description-button/index.tsx
@@ -12,7 +12,7 @@ const DescriptionButton: FC<IDescriptionButtonProps> = ({onClick}) => {
   const {description} = task;
 
   if (description) return <div className="description-text" onClick={onClick} dangerouslySetInnerHTML={{__html: description}}></div>;
-  else return <Input className="border-0 bg-slate-100" onClick={onClick} placeholder="Add a more detailed description..." readOnly />;
+  else return <Input className="border-0 bg-slate-100" onClick={onClick} placeholder="Add a description..." readOnly />;
 };
 
 export default DescriptionButton;
