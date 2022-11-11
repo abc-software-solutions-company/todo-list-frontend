1881bd21e47206cdfa3ad4da6709b3fc703d833c
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-11-11 13:38:51 +0700

    fix style content description

--- src/components/task-detail/task-body/body-left/description/description-button/index.tsx
+++ src/components/task-detail/task-body/body-left/description/description-button/index.tsx
@@ -11,7 +11,7 @@ const DescriptionButton: FC<IDescriptionButtonProps> = ({onClick}) => {
   const {task} = useTask();
   const {description} = task;
 
-  if (description) return <div className="description-text prose" onClick={onClick} dangerouslySetInnerHTML={{__html: description}}></div>;
+  if (description) return <div className="description-text" onClick={onClick} dangerouslySetInnerHTML={{__html: description}}></div>;
   else return <Input className="border-0 bg-slate-100" onClick={onClick} placeholder="Add a more detailed description..." readOnly />;
 };
 
