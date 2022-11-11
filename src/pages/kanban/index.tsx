3e8c85d8bfb146b44846fc1bf22006a3427dc270
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-11-11 00:20:14 +0000

    save to go to company

--- src/pages/kanban/index.tsx
+++ src/pages/kanban/index.tsx
@@ -1,9 +1,14 @@
+import dynamic from 'next/dynamic';
 import React from 'react';
+
 const Kanban = dynamic(() => import('@/components/common/kanban'), {
-  ssr: false,
-})
-export default function Kanban() {
-  return <div>Kanabn
-    <Kanban/>
-  </div>;
+  ssr: false
+});
+export default function KanbanPage() {
+  return (
+    <div>
+      Kanabn
+      <Kanban />
+    </div>
+  );
 }
