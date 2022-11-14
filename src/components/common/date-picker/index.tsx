353336f1f8ee8809ffab62d0c0b63dea0dd72974
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-11-14 08:46:16 +0700

    style for component

--- src/components/common/date-picker/index.tsx
+++ src/components/common/date-picker/index.tsx
@@ -1,4 +1,3 @@
-import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
 import {DesktopDatePicker} from '@mui/x-date-pickers/DesktopDatePicker';
@@ -7,30 +6,36 @@ import {MobileDatePicker} from '@mui/x-date-pickers/MobileDatePicker';
 import dayjs, {Dayjs} from 'dayjs';
 import {FC, useState} from 'react';
 
+import style from './styles.module.scss';
+
 const DatePicker: FC = () => {
   const [value, setValue] = useState<Dayjs | null>(dayjs('2014-08-18T21:11:54'));
   const handleChange = (newValue: Dayjs | null) => {
     setValue(newValue);
   };
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <Stack spacing={3}>
-        <DesktopDatePicker
-          label="Date desktop"
-          inputFormat="MM/DD/YYYY"
-          value={value}
-          onChange={handleChange}
-          renderInput={params => <TextField {...params} />}
-        />
-        <MobileDatePicker
-          label="Date mobile"
-          inputFormat="MM/DD/YYYY"
-          value={value}
-          onChange={handleChange}
-          renderInput={params => <TextField {...params} />}
-        />
-      </Stack>
-    </LocalizationProvider>
+    <div className={style['date-picker']}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <div className="date-picker-desktop">
+          <DesktopDatePicker
+            label="Date desktop"
+            inputFormat="MM/DD/YYYY"
+            value={value}
+            onChange={handleChange}
+            renderInput={params => <TextField {...params} />}
+          />
+        </div>
+        <div className="date-picker-mobile">
+          <MobileDatePicker
+            label="Date mobile"
+            inputFormat="MM/DD/YYYY"
+            value={value}
+            onChange={handleChange}
+            renderInput={params => <TextField {...params} />}
+          />
+        </div>
+      </LocalizationProvider>
+    </div>
   );
 };
 
