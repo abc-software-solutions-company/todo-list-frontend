5ab6b22286171f10c7228c0bab948cedd6cc8d65
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-11-14 10:16:33 +0700

    update ui to match figma

--- src/components/common/date-picker/index.tsx
+++ src/components/common/date-picker/index.tsx
@@ -1,45 +1,27 @@
 import TextField from '@mui/material/TextField';
 import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
-import {DesktopDatePicker} from '@mui/x-date-pickers/DesktopDatePicker';
 import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
 import {MobileDatePicker} from '@mui/x-date-pickers/MobileDatePicker';
 import dayjs, {Dayjs} from 'dayjs';
-import {FC, useState} from 'react';
+import {useState} from 'react';
 
 import style from './styles.module.scss';
 
 interface IDatePickerProp {
   data: string;
-  handle: () => void;
+  onChange: (data?: string) => void;
 }
 
-const DatePicker: FC = ({data, handle}: IDatePickerProp) => {
+const DatePicker = ({data, onChange}: IDatePickerProp) => {
   const [value, setValue] = useState<Dayjs | null>(dayjs(data || '2014-08-18T21:11:54'));
   const handleChange = (newValue: Dayjs | null) => {
     setValue(newValue);
-    handle();
+    onChange(value?.toString());
   };
   return (
     <div className={style['date-picker']}>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <div className="date-picker-desktop">
-          <DesktopDatePicker
-            label="Date desktop"
-            inputFormat="MM/DD/YYYY"
-            value={value}
-            onChange={handleChange}
-            renderInput={params => <TextField {...params} />}
-          />
-        </div>
-        <div className="date-picker-mobile">
-          <MobileDatePicker
-            label="Date mobile"
-            inputFormat="MM/DD/YYYY"
-            value={value}
-            onChange={handleChange}
-            renderInput={params => <TextField {...params} />}
-          />
-        </div>
+        <MobileDatePicker className="box" inputFormat="MM/DD/YYYY" value={value} onChange={handleChange} renderInput={params => <TextField {...params} />} />
       </LocalizationProvider>
     </div>
   );
--- src/components/task-detail/task-body/body-right/date/start-date/index.tsx
+++ src/components/task-detail/task-body/body-right/date/start-date/index.tsx
@@ -1,13 +1,31 @@
 import classNames from 'classnames';
 import {FC} from 'react';
+import {Controller, SubmitHandler, useForm} from 'react-hook-form';
 
+import DatePicker from '@/components/common/date-picker';
 import {IBaseProps} from '@/types';
 
-const StartDate: FC<IBaseProps> = ({className}) => {
+interface IProps extends IBaseProps {
+  startDate?: string;
+}
+
+const StartDate: FC<IProps> = ({className, startDate}) => {
+  const {handleSubmit, control} = useForm<IProps>();
+  const onSubmit: SubmitHandler<IProps> = data => alert(data.startDate);
+
   return (
     <div className={classNames('start-date', className)}>
       <p className="title">Start date</p>
-      <p>3/11/2022</p>
+      {/* <p>3/11/2022</p> */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="startDate"
+          rules={{required: true}}
+          control={control}
+          render={({field}) => <DatePicker data={startDate || ''} onChange={text => field.onChange(text)} />}
+        />
+        <input type="submit" />
+      </form>
     </div>
   );
 };
