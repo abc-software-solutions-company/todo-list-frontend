f3266d89e5024857de0c554b6794504e335ca236
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-11-22 10:58:12 +0700

    backup

--- src/components/common/date-picker/index.tsx
+++ src/components/common/date-picker/index.tsx
@@ -1,7 +1,7 @@
 import TextField from '@mui/material/TextField';
 import {DateTimePicker} from '@mui/x-date-pickers';
 import dayjs, {Dayjs} from 'dayjs';
-import {useEffect, useRef, useState} from 'react';
+import {useState} from 'react';
 
 import style from './styles.module.scss';
 
@@ -13,35 +13,23 @@ interface IDatePickerProp {
   minDateTime?: Date;
 }
 
-const useOutsideAlerter = (ref: any) => {
-  useEffect(() => {
-    const handleClickOutside = (event: Event) => {
-      if (ref.current && !ref.current.contains(event.target)) {
-        alert('You clicked outside of me!');
-      } else {
-        alert('You click inside of me');
-      }
-    };
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [ref]);
-};
-
 const DatePicker = ({value, onChange, readonly, title, minDateTime}: IDatePickerProp) => {
+  const inputFormat = 'MM/DD/YYYY HH:MM';
   const [day, setDay] = useState<Dayjs | null>(dayjs(value));
+  const [active, setActive] = useState(false);
   const handleChange = (newDay: Dayjs | null) => {
     setDay(newDay);
   };
 
-  const wrapperRef = useRef(null);
-  useOutsideAlerter(wrapperRef);
+  const placeholderChange = () => {
+    return active ? 'MM/DD/YYYY' : 'None';
+  };
+
   return (
-    <div className={style['date-time-picker']} ref={wrapperRef}>
+    <div className={style['date-time-picker']}>
       <DateTimePicker
-        className="date-input"
-        inputFormat={'MM/DD/YYYY HH:MM'}
+        className={`date-input ${active && 'active'}`}
+        inputFormat={inputFormat}
         showToolbar={true}
         value={day}
         toolbarTitle={title}
@@ -51,11 +39,17 @@ const DatePicker = ({value, onChange, readonly, title, minDateTime}: IDatePicker
         onAccept={() => onChange(day?.toDate())}
         renderInput={params => (
           <TextField
+            onBlur={() => {
+              setActive(false);
+            }}
+            onClick={() => {
+              setActive(true);
+            }}
             focused={false}
             {...params}
             inputProps={{
               ...params.inputProps,
-              placeholder: 'None'
+              placeholder: placeholderChange()
             }}
           />
         )}
