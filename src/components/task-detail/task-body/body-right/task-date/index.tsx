025942db7ff203a2e18c9ff7e69fdb9843e94c64
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-12-01 11:04:37 +0700

    add compare date in task date

--- src/components/task-detail/task-body/body-right/task-date/index.tsx
+++ src/components/task-detail/task-body/body-right/task-date/index.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
 import {FC, HTMLAttributes, useState} from 'react';
 
+import useToast from '@/core-ui/toast';
 import api from '@/data/api';
 import useTask from '@/states/task/use-task';
 
@@ -8,11 +9,23 @@ import PickDateTime from './pick-date-time';
 
 const TaskDate: FC<HTMLAttributes<HTMLDivElement>> = ({className}) => {
   const {task, write} = useTask();
+  const toast = useToast();
+
   const [minDate, setMinDate] = useState<Date>(task.startDate);
+
   const handleSaveDueDate = (date: Date) => {
     api.task.update({id: task.id, dueDate: date});
   };
+
   const handleSaveStartDate = (date: Date) => {
+    const start = new Date(date.toISOString());
+    const due = new Date(task.dueDate ? task.dueDate.toString() : '1/1/1990');
+
+    if (start > due && task.dueDate) {
+      toast.show({type: 'danger', title: 'Error', content: 'Start Date cannot over Due Date'});
+      return;
+    }
+
     api.task.update({id: task.id, startDate: date}).then(() => setMinDate(date));
   };
 
