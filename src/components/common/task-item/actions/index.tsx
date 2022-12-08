217f3a8f46393c90257fe4911bcac57c0accb5a4
phuocvuive09 <a000014@abcsoftwarecompany.com>
2022-12-08 13:12:38 +0700

    Recover code

--- src/components/common/task-item/actions/index.tsx
+++ src/components/common/task-item/actions/index.tsx
@@ -82,6 +82,8 @@ const Actions: FC<IActionsProps> = ({task, todolist, write = false}) => {
           assigneeList
         }}
         readonly={write}
+        sx={{position: 'absolute'}}
+        hideIconWhenClick={false}
       />
       <div className="piority">
         <TaskPiority task={task} readOnly={!write} onChange={onChangePriority} hideTitle={true} />
