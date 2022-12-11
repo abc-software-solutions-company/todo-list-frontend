de14af97fc36b285c51e494415dff26b6b3664ac
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-12-11 22:28:11 +0700

    add auto reload when deploy new content to server

---
+++ src/components/common/deploy-restart/index.tsx
@@ -0,0 +1,26 @@
+import {useRouter} from 'next/router';
+import {ReactNode, useEffect} from 'react';
+
+import LocalStorage from '@/utils/local-storage';
+
+interface IDeployRestartProp {
+  children: ReactNode;
+}
+
+export default function DeployRestart({children}: IDeployRestartProp) {
+  const router = useRouter();
+  const serverBuildID = process.env.NEXT_PUBLIC_GIT_COMMIT_SHA || 'serverID';
+  const clientBuildID = (typeof window !== 'undefined' && LocalStorage.buildID.get()) || 'clientID';
+  useEffect(() => {
+    console.log(
+      '🚀 ~ file: index.tsx:16 ~ useEffect ~ clientBuildID !== serverBuildID',
+      clientBuildID !== serverBuildID
+    );
+    if (clientBuildID !== serverBuildID) {
+      LocalStorage.buildID.set(serverBuildID);
+      router.reload();
+    }
+  }, [clientBuildID, serverBuildID]);
+
+  return <>{children}</>;
+}
