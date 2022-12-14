a62481fc8ff87dd031e2c2111cac5aa462f5609b
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-12-14 13:41:38 +0700

    make layout can view first

--- src/components/kanban/index.tsx
+++ src/components/kanban/index.tsx
@@ -1,38 +1,47 @@
-import {FC} from 'react';
+import {useRouter} from 'next/router';
+import {FC, useEffect} from 'react';
+
+import useTodolist from '@/states/todolist/use-todolist';
 
 interface IProps {
   id: string;
 }
 const Kanban: FC<IProps> = () => {
-  return null;
-  // const {todolist, initial} = useTodolist();
-  // useEffect(() => {
-  //   initial(id);
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
-  // if (!todolist) return null;
-  // const columns = todolist.status;
-  // return (
-  //   <div className="container my-5 grid auto-cols-fr grid-flow-col">
-  //     {columns.length > 0 &&
-  //       columns.map(status => {
-  //         return (
-  //           <div key={status.id}>
-  //             <div className="border p-2 text-center">{status.name}</div>
-  //             <div>
-  //               {status.tasks.map(task => {
-  //                 return (
-  //                   <div key={task.id} className="border p-2 text-center">
-  //                     {task.name}
-  //                   </div>
-  //                 );
-  //               })}
-  //             </div>
-  //           </div>
-  //         );
-  //       })}
-  //   </div>
-  // );
+  const {todolist, initial, error} = useTodolist();
+  console.log('🚀 ~ file: index.tsx:9 ~ todolist', todolist);
+  const router = useRouter();
+  const {id} = router.query;
+  useEffect(() => {
+    initial(id as string);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (!todolist || error) return null;
+  const columns = todolist.status;
+
+  return (
+    <div className="container my-5 grid auto-cols-fr grid-flow-col">
+      {columns.length > 0 &&
+        columns.map(status => {
+          return (
+            <div key={status.id}>
+              <div className="border p-2 text-center">{status.name}</div>
+              <div>
+                {todolist.tasks
+                  .filter(x => x.statusId == status.id)
+                  .map(task => {
+                    return (
+                      <div key={task.id} className="border p-2 text-center">
+                        {task.name}
+                      </div>
+                    );
+                  })}
+              </div>
+            </div>
+          );
+        })}
+    </div>
+  );
 };
 
 export default Kanban;
