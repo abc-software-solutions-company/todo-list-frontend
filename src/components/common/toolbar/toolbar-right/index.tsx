af577377eec1880156d165ffcded6b04b1833fd2
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-12-29 07:18:07 +0700

    add button to switch kanban and board in toolbar right new kanban page

--- src/components/common/toolbar/toolbar-right/index.tsx
+++ src/components/common/toolbar/toolbar-right/index.tsx
@@ -1,16 +1,30 @@
+import {useRouter} from 'next/router';
 import {FC} from 'react';
 
+import {ROUTES} from '@/configs/routes.config';
 import Icon from '@/core-ui/icon';
 
 const ToolBarRight: FC = () => {
+  const router = useRouter();
+  const {id} = router.query;
   return (
     <div className="toolbar-right flex gap-x-7">
       <div className="view-mode flex gap-x-2">
         <div className="list-view">
-          <Icon name="list-view" className="ico-vertical leading-tight" size={16} />
+          <Icon
+            name="list-view"
+            className="ico-vertical leading-tight hover:cursor-pointer"
+            size={16}
+            onClick={() => router.push(`${ROUTES.LIST}/${id}`)}
+          />
         </div>
         <div className="kanban-view">
-          <Icon name="horizontal" className="ico-horizontal leading-tight" size={16} />
+          <Icon
+            name="horizontal"
+            className="ico-horizontal leading-tight hover:cursor-pointer"
+            size={16}
+            onClick={() => router.push(`${ROUTES.KANBAN}/${id}`)}
+          />
         </div>
       </div>
       <div className="sort flex gap-x-1">
