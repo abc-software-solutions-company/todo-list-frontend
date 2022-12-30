3020fbde93044859ba773913a7b3cf940d39b5c9
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-12-30 13:26:11 +0700

    detect add task kanban on the end of column

--- src/components/board/column/body/index.tsx
+++ src/components/board/column/body/index.tsx
@@ -18,22 +18,32 @@ export default function KanbanColumnBody({id, tasks = []}: IKanbanColumnBody) {
   const {setNodeRef} = useDroppable({id});
 
   const [windowHeight, setWindowHeight] = useState(750);
+  const [showFloatBtn, setShowFloatBtn] = useState(false);
+
+  const checkScrollBar = () => {
+    const columnElm = document.querySelector(`#column-body-${id}`);
+    if (columnElm && columnElm.scrollHeight > columnElm.clientHeight) {
+      setShowFloatBtn(true);
+    }
+  };
 
   useEffect(() => {
     if (window) {
       window.addEventListener('resize', () => {
         if (windowHeight > 0) setWindowHeight(window.innerHeight * 0.7);
       });
+      checkScrollBar();
     }
   }, []);
 
   return (
     <SortableContext id={id} items={tasks} strategy={verticalListSortingStrategy}>
-      <ul className={style['column-body']} ref={setNodeRef}>
+      <ul className={style['column-body']} id={`column-body-${id}`} ref={setNodeRef}>
         {tasks.map((task, idx: number) => (
           <KanbanTaskItem key={idx} task={task} />
         ))}
         {/* <AddTaskKanban id={Number(id)} /> */}
+        {showFloatBtn ? 'yes' : 'no'}
       </ul>
     </SortableContext>
   );
