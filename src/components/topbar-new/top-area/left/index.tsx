d2e59bf05b619225984050343062c4d743a5b71e
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-01-02 10:59:00 +0700

    layout for top area left

--- src/components/topbar-new/top-area/left/index.tsx
+++ src/components/topbar-new/top-area/left/index.tsx
@@ -1,15 +1,24 @@
 import {FC} from 'react';
 
+import Icon from '@/core-ui/icon';
+import {MUI_ICON} from '@/utils/mui-icon';
+
 import style from './style.module.scss';
 
 const TopAreaLeft: FC = () => {
   return (
     <div className={style['top-area-left']}>
-      <div className="decor">Decorator</div>
-      <div className="page-name">Page Name</div>
+      <div className="decor">
+        <Icon name="decor" className="ico-three-line text-white" />
+      </div>
+      <div className="page-title">To-do list tasks</div>
       <div className="page-action">
-        <div className="favorite">Favorite</div>
-        <div className="more">More</div>
+        <div className="favorite">
+          <Icon name="favorite" className="ico-star  text-[#38BDF8]" />
+        </div>
+        <div className="more">
+          <MUI_ICON.MORE_VERT />
+        </div>
       </div>
     </div>
   );
