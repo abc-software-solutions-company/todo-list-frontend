c2afc7e49c99a061a569340a95eddf82f792e7be
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-01-02 11:23:02 +0700

    improve new ui for topbar

--- src/components/topbar-new/top-area/right/index.tsx
+++ src/components/topbar-new/top-area/right/index.tsx
@@ -1,13 +1,20 @@
 import {FC} from 'react';
 
+import Notification from '@/components/notification';
+
+import TopBarAccount from './account';
 import style from './style.module.scss';
 
 const TopAreaRight: FC = () => {
   return (
     <div className={style['top-area-right']}>
       <div className="share">Share</div>
-      <div className="notification">Notification</div>
-      <div className="account">Account</div>
+      <div className="notification">
+        <Notification />
+      </div>
+      <div className="account">
+        <TopBarAccount />
+      </div>
     </div>
   );
 };
