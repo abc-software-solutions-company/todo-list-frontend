b947fb12e9dc71ee807f7656dd2f64f5445bcd6c
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-01-03 13:18:52 +0700

    detect share button well

--- src/components/topbar-new/top-area/right/index.tsx
+++ src/components/topbar-new/top-area/right/index.tsx
@@ -4,11 +4,11 @@ import {FC} from 'react';
 import Tool, {IToolProps} from '@/components/lists-detail/toolbar/tool';
 import ToolMenu from '@/components/lists-detail/toolbar/tool-menu';
 import Notification from '@/components/notification';
-import {ROUTES} from '@/configs/routes.config';
 import Icon from '@/core-ui/icon';
 import useBoards from '@/states/board/use-boards';
 import useModals from '@/states/modals/use-modals';
 import useTodolist from '@/states/todolist/use-todolist';
+import {isBoardPage, isListDetailPage} from '@/utils/check-routes';
 import {MUI_ICON} from '@/utils/mui-icon';
 
 import TopBarAccount from './account';
@@ -18,12 +18,16 @@ import style from './style.module.scss';
 const TopAreaRight: FC = () => {
   const {boardData} = useBoards();
   const {todolist} = useTodolist();
+
   const router = useRouter();
+  const path = router.asPath;
+  const {id} = router.query;
+
   const {setSelectedTodolist, setIsOpenModal} = useModals();
 
   const onShare = () => {
-    if (router.asPath.includes(`${ROUTES.LIST}`)) setSelectedTodolist(todolist);
-    if (router.asPath.includes(`${ROUTES.KANBAN}`)) setSelectedTodolist(boardData);
+    if (isListDetailPage(path, id as string)) setSelectedTodolist(todolist);
+    if (isBoardPage(path, id as string)) setSelectedTodolist(boardData);
     setIsOpenModal('shareList');
   };
 
@@ -38,7 +42,7 @@ const TopAreaRight: FC = () => {
   ));
   return (
     <div className={style['top-area-right']}>
-      <TopBarShare onShare={onShare} />
+      {(isBoardPage(path, id as string) || isListDetailPage(path, id as string)) && <TopBarShare onShare={onShare} />}
       <div className="notification">
         <Notification />
       </div>
