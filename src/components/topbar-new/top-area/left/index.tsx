25bbd7fd381495dca8dcd07e9d351f9cfa82f3b6
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-01-03 13:21:08 +0700

    fix code to detect start icon bettern

--- src/components/topbar-new/top-area/left/index.tsx
+++ src/components/topbar-new/top-area/left/index.tsx
@@ -2,21 +2,20 @@ import {useRouter} from 'next/router';
 import {FC, useEffect, useState} from 'react';
 
 import TodolistFavorite from '@/components/common/todolist-favorite';
-import {ROUTES} from '@/configs/routes.config';
 import Icon from '@/core-ui/icon';
 import useBoards from '@/states/board/use-boards';
 import useTodolist from '@/states/todolist/use-todolist';
+import {isBoardPage, isListDetailPage} from '@/utils/check-routes';
 import {MUI_ICON} from '@/utils/mui-icon';
 
 import style from './style.module.scss';
 
 const TopAreaLeft: FC = () => {
-  const router = useRouter();
-  const {id} = router.query;
   const {todolist} = useTodolist();
   const {boardData} = useBoards();
-  const isBoardPage = router.asPath.includes(ROUTES.KANBAN);
-  const isListPage = router.asPath.includes(`${ROUTES.LIST}/${id}`);
+  const router = useRouter();
+  const path = router.asPath;
+  const {id} = router.query;
   const [pageTitle, setPageTitle] = useState('');
   useEffect(() => {
     if (typeof window !== undefined) {
@@ -37,8 +36,8 @@ const TopAreaLeft: FC = () => {
       <div className="page-title">{pageTitle}</div>
       <div className="page-action">
         <div className="favorite-list">
-          {todolist && isListPage && <TodolistFavorite id={todolist.id} favorite={todolist.favorite} />}
-          {boardData && isBoardPage && <TodolistFavorite id={boardData.id} favorite={boardData.favorite} />}
+          {isListDetailPage(path, id as string) && <TodolistFavorite id={todolist.id} favorite={todolist.favorite} />}
+          {isBoardPage(path, id as string) && <TodolistFavorite id={boardData.id} favorite={boardData.favorite} />}
         </div>
         <div className="more">
           <MUI_ICON.MORE_VERT />
