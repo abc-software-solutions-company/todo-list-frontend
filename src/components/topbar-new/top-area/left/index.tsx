348c34658018476a8606da42edc45e48bf4e3164
Thiện Lâm <lamminhthien02012000@gmail.com>
2023-01-06 11:19:39 +0700

    split code hook for toparea-left

--- src/components/topbar-new/top-area/left/index.tsx
+++ src/components/topbar-new/top-area/left/index.tsx
@@ -1,64 +1,16 @@
-import {useRouter} from 'next/router';
-import {FC, useEffect, useState} from 'react';
+import {FC} from 'react';
 
 import Back from '@/components/common/back';
 import TodolistFavorite from '@/components/common/todolist-favorite';
 import {ROUTES} from '@/configs/routes.config';
 import Icon from '@/core-ui/icon';
-import useBoards from '@/states/board/use-boards';
-import useTodolist from '@/states/todolist/use-todolist';
 import {isBoardPage, isListDetailPage} from '@/utils/check-routes';
-import LocalStorage from '@/utils/local-storage';
 
+import useTopAreaLeft from './hook';
 import style from './style.module.scss';
 
 const TopAreaLeft: FC = () => {
-  const {todolist} = useTodolist();
-  const {boardData} = useBoards();
-  const router = useRouter();
-
-  const path = router.asPath;
-  const currentPage = router.pathname;
-  const {id} = router.query;
-
-  const [pageTitle, setPageTitle] = useState('');
-
-  const returnTo = (curPage: string) => {
-    const checkPage = LocalStorage.checkPage.get();
-    switch (curPage) {
-      case `${ROUTES.LIST}`:
-        router.push(ROUTES.HOME);
-        break;
-      case `${ROUTES.TASK}`:
-        router.push(ROUTES.LIST);
-        break;
-      case `${ROUTES.LIST}/[id]`:
-        if (checkPage === '/tasks') router.push(ROUTES.TASK);
-        else router.push(ROUTES.LIST);
-        break;
-      case `${ROUTES.TASK}/[id]`:
-        if (checkPage === '/lists') {
-          router.push(ROUTES.LIST + '/' + LocalStorage.listId.get());
-        } else if (checkPage === '/tasks') {
-          router.push(ROUTES.TASK);
-        }
-        break;
-      default:
-        router.back();
-    }
-  };
-
-  const removeNotificationNumber = () => {
-    return document.title.slice(0, document.title.lastIndexOf('('));
-  };
-
-  useEffect(() => {
-    setPageTitle(removeNotificationNumber);
-    router.events.on('routeChangeComplete', () => {
-      setPageTitle(removeNotificationNumber);
-    });
-  }, []);
-
+  const {boardData, currentPage, id, pageTitle, path, returnTo, todolist} = useTopAreaLeft();
   return (
     <div className={style['top-area-left']}>
       <div className="decor">
