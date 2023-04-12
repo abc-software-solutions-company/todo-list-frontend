f213c7e5d5411c85a11b39d10634e307940e053f
LeNhi-inter <95989630+NhiLe11@users.noreply.github.com>
2023-04-12 14:08:38 +0700

    ,

--- src/components/modal/documents/types-create.ts
+++ src/components/modal/documents/types-create.ts
@@ -1,10 +1,7 @@
-// import {IDocumentAttribute} from '@/data/api/types/documents.type';
-
 export interface IProps {
   open: boolean;
   onClose: () => void;
   docChild?: boolean;
-  // data: IDocumentAttribute;
 }
 
 export interface IFormInputs {
