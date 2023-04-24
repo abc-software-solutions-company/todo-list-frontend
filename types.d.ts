c820be1210b0c8f72a851301a2cf6fcf479b4ce8
LeNhi-inter <95989630+NhiLe11@users.noreply.github.com>
2023-04-24 13:53:16 +0700

    Update types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -19,4 +19,3 @@ declare module '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 
 declare module 'glightbox';
 declare module 'glightbox/dist/css/glightbox.min.css';
-declare module 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
