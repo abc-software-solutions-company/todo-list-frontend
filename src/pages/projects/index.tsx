fa13aaf10a6940927a9c6951651b052a2578610b
Huỳnh Công Lợi <congloi3114@gmail.com>
2023-12-26 16:12:46 +0700

    map-list-task-card

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -3,7 +3,15 @@ import React from 'react';
 
 import NewLayout from '@/layouts/new-layout';
 
+import TaskCard from './project/project-card';
+
 export default function ProjectsPage() {
+  const testData = [
+    {title: 'Task 1', dueDate: 'March 30,2023', completedTaskCount: 15, totalTaskCount: 20},
+    {title: 'Task 2', dueDate: 'April 15,2023', completedTaskCount: 10, totalTaskCount: 15},
+    {title: 'Task 3', dueDate: 'May 1,2023', completedTaskCount: 5, totalTaskCount: 10}
+  ];
+
   return (
     <>
       <div className="mt-8 mb-4 flex items-center font-bold">
@@ -15,6 +23,16 @@ export default function ProjectsPage() {
           <SlidersHorizontal size={19} className="mr-2" /> Sell all
         </button>
       </div>
+
+      {testData.map((task, index) => (
+        <TaskCard
+          key={index}
+          title={task.title}
+          dueDate={task.dueDate}
+          completedTaskCount={task.completedTaskCount}
+          totalTaskCount={task.totalTaskCount}
+        />
+      ))}
     </>
   );
 }
