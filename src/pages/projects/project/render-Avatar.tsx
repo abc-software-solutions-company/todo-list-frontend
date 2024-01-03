d53b65c4256a6c3f2e910e2d691b917f12e79d3a
MaiAnhisreal <anhnguyen14102022@gmail.com>
2024-01-03 09:19:16 +0700

    Update render Avatar

--- src/pages/projects/project/render-Avatar.tsx
+++ src/pages/projects/project/render-Avatar.tsx
@@ -13,32 +13,34 @@ const Avatars: FC<AvatarsProps> = ({names}) => {
     'absolute left-[64px] z-50 h-[32px] w-[32px] shrink-0 rounded-full bg-blue-500'
   ];
 
-  const avatars = names.map((name, index) => {
-    if (index < 5) {
-      const avatarStyle = avatarStyles[index];
-      const avatarInitial = name.charAt(0).toUpperCase();
+  return (
+    <div>
+      {names.map((name, index) => {
+        if (index < 5) {
+          const avatarStyle = avatarStyles[index];
+          const avatarInitial = name.charAt(0).toUpperCase();
 
-      return (
-        <div key={index} className={avatarStyle}>
-          <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform text-center text-white">
-            {avatarInitial}
-          </p>
-        </div>
-      );
-    } else if (index === 5) {
-      return (
-        <div
-          key={index}
-          className="absolute left-[80px] z-[60] flex h-[32px] w-[32px] shrink-0 items-center justify-center rounded-full bg-white"
-        >
-          <p className="absolute"> +{name.length - index}</p>
-        </div>
-      );
-    }
-    return null;
-  });
-
-  return <div>{avatars}</div>;
+          return (
+            <div key={index} className={avatarStyle}>
+              <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform text-center text-white">
+                {avatarInitial}
+              </p>
+            </div>
+          );
+        } else if (index === 5) {
+          return (
+            <div
+              key={index}
+              className="absolute left-[80px] z-[60] flex h-[32px] w-[32px] shrink-0 items-center justify-center rounded-full bg-white"
+            >
+              <p className="absolute"> +{name.length - index}</p>
+            </div>
+          );
+        }
+        return null;
+      })}
+    </div>
+  );
 };
 
 export default Avatars;
